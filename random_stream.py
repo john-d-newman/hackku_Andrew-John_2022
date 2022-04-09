942b1f3e694cc68ecd1bf5450d18a0c82dacc2ea
john-d-newman <100106039+john-d-newman@users.noreply.github.com>
2022-04-09 12:19:42 -0500

    Barebones python

--- random_stream.py
+++ random_stream.py
@@ -8,7 +8,7 @@ Desc:  This file parses a csv file of stream names and tags
 '''
 from os import link
 import random
-
+from flask import render_template
 from flask import Flask
 from datetime import datetime
 import re
@@ -17,34 +17,39 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "This is the main site, naivagate to other sites."
 
+'''
+@app.route("/hello/")
 @app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+'''
 
 
+@app.route("/link_from_tag/")
 @app.route("/link_from_tag/<tag>")
-def link_from_tag(tag):
+def link_from_tag(tag = None):
+    tag = tag
+    if tag == None:
+        return render_template(
+            'rand_1.html',
+            tag=tag,
+            link = 'https://youtu.be/dQw4w9WgXcQ'
+        )
+
     stream_data = open('stream_database.csv')
 
-    return_stream = open('desired_stream.txt','w')
-    return_stream_list = []
+   # return_stream = open('desired_stream.txt','w')
 
+    return_stream_list = []
+    tag_list = []
     operating_list = []
+
     for item in stream_data:
         operating_list.append(item.split(','))
     operating_list.pop(0)
@@ -52,37 +57,26 @@ def link_from_tag(tag):
         if '\n' in item[1]:
             item[1]= item[1][0:len(item[1])-1]
 
-    print(operating_list)
     
     for item in operating_list:
         if tag in item:
+            tag_list.append(item[0])
             return_stream_list.append(item[1])
 
-    for item in return_stream_list:
-        return_stream.write(str(item)+'\n')
+#    for item in return_stream_list:
+#       return_stream.write(str(item)+'\n')
 
     stream_data.close()
-    return_stream.close()
-    return str(return_stream_list)
-
-
-#link_from_tag('elephant')
-
-@app.route('/random_stream/')
-def choose_rand_stream(stream_data_filename):
-    stream_option = []
-    file_object = open(str(stream_data_filename),'r')
-
-    for line in file_object:
-        stream_option.append(line[0:len(line)-1])
-
-    print(stream_option)
-    length = len(stream_option)-1
-    print(length)
-
-    file_object.close()
-
-    return stream_option[random.randint(0,length)]
+#    return_stream.close()
+    if tag in tag_list:
+        return render_template(
+        'rand_1.html',
+        tag=tag,
+        link = return_stream_list[random.randint(0,len(return_stream_list)-1)]
+        )
+    else:
+        return "Tag not in options, try retyping or clicking an image"
+    
 
     
 
