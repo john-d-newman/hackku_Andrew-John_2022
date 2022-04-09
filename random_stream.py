f3fafa512ce27b4829bbecf1421adf9a15261316
john-d-newman <100106039+john-d-newman@users.noreply.github.com>
2022-04-09 16:12:01 -0500

    base site python completed
    Now we just need the html sites to add the scripts to

--- random_stream.py
+++ random_stream.py
@@ -15,9 +15,41 @@ import re
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/<hold>")
 def home():
-    return "This is the main site, naivagate to other sites."
+    stream_data = open('stream_database.csv','r')
+    
+    stream_list = []
+    tag_list = []
+    operating_list = []
+
+    for item in stream_data:
+        operating_list.append(item.split(','))
+    
+    print(operating_list)
+    for item in operating_list:
+        if '\n' in item[len(item)-1]:
+            item[len(item)-1]= item[len(item)-1][0:len(item[len(item)-1])-1]
+
+    print(operating_list)
+    
+    rand_list = []
+    for _ in range(4):
+        rand_list.append(random.randint(0,len(stream_list)-1))
+
+
+
+    tag_list = tag_list[0]
+    stream_data.close()
+
+    return render_template(
+        'main.html',
+        link1 = stream_data[rand_list[0]],
+        link2 = stream_data[rand_list[1]],
+        link3 = stream_data[rand_list[2]],
+        link4 = stream_data[rand_list[3]]
+
+    )
 
 '''
 @app.route("/hello/")
@@ -42,7 +74,7 @@ def link_from_tag(tag = None):
             link = 'https://youtu.be/dQw4w9WgXcQ'
         )
 
-    stream_data = open('stream_database.csv')
+    stream_data = open('stream_database.csv','r')
 
     return_stream_list = []
     tag_list = []
@@ -50,7 +82,7 @@ def link_from_tag(tag = None):
 
     for item in stream_data:
         operating_list.append(item.split(','))
-    operating_list.pop(0)
+    
     print(operating_list)
     for item in operating_list:
         if '\n' in item[len(item)-1]:
@@ -86,5 +118,85 @@ def link_from_tag(tag = None):
         return "Tag not in options, try retyping or clicking an image"
     
 
+
+
+#@app.route('/addstream/')
+def addstream(stream_link,csvtags):
+    stream_file = open('stream_database.csv','a')
+    stream_file.write(str(stream_link) +',' + csvtags)
+    stream_file.close()
+
+#@app.route('/addtags/')
+def addtags(stream_link, csvtags):
+    stream_file = open('stream_database.csv','r')
+
+    index = 0
+    operating_list = []
+    tag_list = []
+    stream_list = []
+    
+    new_tags = csvtags.split(',')
+
+    for item in stream_file:
+        operating_list.append(item.split(','))
     
 
+    for item in operating_list:
+        if '\n' in item[len(item)-1]:
+            item[len(item)-1]= item[len(item)-1][0:len(item[len(item)-1])-1]
+
+    for item in operating_list:
+        tag_list.append(item[1:len(item)])
+        stream_list.append(item[0])
+
+    
+    
+    print(stream_list)
+
+    if stream_link in stream_list:
+        for item in stream_list:
+            if stream_link != item:
+                index += 1
+            else:
+                findex = index
+    else:
+        print("THIS WILL BE A RETURN FUNCITON WITH AN ERROR")
+        return("Link not defined, try the add streams link")
+
+    tag_list = tagcleaner(tag_list)
+
+    for item in new_tags:
+        tag_list[findex].append(item)
+
+    stream_file.close()
+    stream_file = open('stream_database.csv','w')
+    index = 0
+
+    for item in stream_list:
+        stream_file.write(item+',')
+        for item in tag_list[index]:
+            stream_file.write(item+',')
+        index += 1
+        stream_file.write('\n')
+
+
+    stream_file.close()
+    print(index)
+    print(findex)
+    print(tag_list)
+    
+def tagcleaner(tag_list):
+    cleaned_tags = []
+    doublec_tags = []
+    for item in tag_list:
+        while '' in item:
+            item.remove('')
+        cleaned_tags.append(item)
+    for item in cleaned_tags:
+        for tag in item:
+            while item.count(tag)>1:
+                item.remove(tag)
+        doublec_tags.append(item)
+    return doublec_tags
+
+addtags('https://zoo.sandiegozoo.org/cams/polar-cam','bear,arctic,paws,pol,pola')
