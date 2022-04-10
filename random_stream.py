b91ae14de12ecccca480d1c22df123976803d325
john-d-newman <100106039+john-d-newman@users.noreply.github.com>
2022-04-09 20:31:03 -0500

    V.01 build
    Everything works, bare bones

--- random_stream.py
+++ random_stream.py
@@ -13,44 +13,67 @@ from flask import Flask
 from datetime import datetime
 import re
 from flask import request
+from flask import redirect
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-    stream_data = open('stream_database.csv','r')
-
-    stream_list = []
-    tag_list = []
-    operating_list = []
-
-    for item in stream_data:
-        operating_list.append(item.split(','))
     
-    print(operating_list)
-    for item in operating_list:
-        if '\n' in item[len(item)-1]:
-            item[len(item)-1]= item[len(item)-1][0:len(item[len(item)-1])-1]
+    raw_tag = request.query_string.decode()
+    tag = ''
 
-    print(operating_list)
+    if raw_tag != '':
+        tag = raw_tag[4:len(raw_tag)]
     
-    rand_list = []
-    for _ in range(4):
-        rand_list.append(random.randint(0,len(stream_list)-1))
+
+    if tag == '' or tag == None:
+
+        stream_data = open('stream_database.csv','r')
+
+        stream_list = []
+        tag_list = []
+        operating_list = []
+
+        for item in stream_data:
+            operating_list.append(item.split(','))
+    
+        print(operating_list)
+        for item in operating_list:
+            if '\n' in item[len(item)-1]:
+                item[len(item)-1]= item[len(item)-1][0:len(item[len(item)-1])-1]
+
+        print(operating_list)
+
+        for item in operating_list:
+            tag_list.append(item[1:len(item)])
+            stream_list.append(item[0])
+    
+        rand_list = []
+        length = len(stream_list)
+        
+        for _ in range(4):
+            rand_list.append(random.randint(0,length-1))
 
 
 
-    tag_list = tag_list[0]
-    stream_data.close()
+        stream_data.close()
 
-    return render_template(
-        'main.html',
-        link1 = stream_data[rand_list[0]],
-        link2 = stream_data[rand_list[1]],
-        link3 = stream_data[rand_list[2]],
-        link4 = stream_data[rand_list[3]]
-
-    )
+        return render_template(
+            'joe.html',
+            link1 = stream_list[rand_list[0]],
+            tag1 = tag_list[rand_list[0]][0],
+            link2 = stream_list[rand_list[1]],
+            tag2 = tag_list[rand_list[1]][0],
+            link3 = stream_list[rand_list[2]],
+            tag3 = tag_list[rand_list[2]][0],
+            link4 = stream_list[rand_list[3]],
+            tag4 = tag_list[rand_list[3]][0]
+            )
+    else:
+        return redirect('/search?'+ raw_tag)
 
 '''
 @app.route("/hello/")
@@ -107,7 +130,7 @@ def link_from_tag(tag = None):
     
     if len(tag_list) == 0:
         return render_template(
-            'joe2.html',
+            'joe3.html',
             tag = tag,
             link = 'https://youtu.be/dQw4w9WgXcQ'
         )
@@ -133,7 +156,68 @@ def link_from_tag(tag = None):
     else:
         return "Tag not in options, try retyping or clicking an image"
     
+def search(tag):
+    raw_tag = request.query_string.decode()
+    if raw_tag != '':
+        tag = raw_tag[4:len(raw_tag)]
+    
 
+    if tag == '' or tag == None:
+        return render_template(
+            'joe2.html',
+            tag=tag,
+            link = 'https://youtu.be/dQw4w9WgXcQ'
+        )
+        
+    stream_data = open('stream_database.csv','r')
+    return_stream_list = []
+    tag_list = []
+    operating_list = []
+
+    for item in stream_data:
+        operating_list.append(item.split(','))
+    
+    print(operating_list)
+    for item in operating_list:
+        if '\n' in item[len(item)-1]:
+            item[len(item)-1]= item[len(item)-1][0:len(item[len(item)-1])-1]
+
+    print(operating_list)
+    
+    for item in operating_list:
+        if tag in item:
+            tag_list.append(item[1:len(item)])
+            return_stream_list.append(item[0])
+
+
+    
+    if len(tag_list) == 0:
+        return render_template(
+            'joe3.html',
+            tag = tag,
+            link = 'https://youtu.be/dQw4w9WgXcQ'
+        )
+    tag_list = tag_list[0]
+
+    print(tag_list[0])
+    print(return_stream_list)
+    print(tag)
+#    for item in return_stream_list:
+#       return_stream.write(str(item)+'\n')
+    for item in tag_list:
+        if tag == item:
+            print("WHYYYYYYYYYYYYY")
+
+    stream_data.close()
+#    return_stream.close()
+    if tag in tag_list:
+        return render_template(
+        'joe2.html',
+        tag=tag_list[0],
+        link = return_stream_list[random.randint(0,len(return_stream_list)-1)]
+        )
+    else:
+        return "Tag not in options, try retyping or clicking an image"
 
 
 #@app.route('/addstream/')
